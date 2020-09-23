bfa2b44928546c39aeea5205f9f8e2f3eb48639f
jrwatson94 <j.watsonreid@gmail.com>
2020-09-23 12:52:06 -0400

    all tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES("Mr. Chocolate",3,"M","brown","somber",TRUE);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES("Rowdy",2,"F","tan","excited",TRUE);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES("Tabitha",4,"F","dark brown","quiet",FALSE);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES("Sergeant Brown",7,"M","brown","aggressive",TRUE);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES("Melissa",1,"F","black","goofy",TRUE);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES("Grinch",8,"M","dark brown","grumpy",FALSE);
+
+INSERT INTO bears (name,age,gender,color,temperament,alive)
+VALUES("Wendy",3,"F","brown","stoic",TRUE);
+
+INSERT INTO bears (age,gender,color,temperament,alive)
+VALUES(3,"M","brown","somber",TRUE);
